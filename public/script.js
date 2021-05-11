53535bc0fd8906517ed98ba920b378b6f9986f3f
farhankhan34 <70713376+farhankhan34@users.noreply.github.com>
2021-05-11 16:05:24 +1000

    Swimlanes added

--- public/script.js
+++ public/script.js
@@ -40,15 +40,16 @@ let aPlaceToShow = document.getElementById("allTasks");
 
 taskList = []; //3.1
 
-function addTask($taskName, $dueDate, $priority, $eta, $completionStatus) {
+function addTask($taskName, $dueDate, $eta, $completionTime, $priority, $completionStatus) {
   var task = {
     taskName: $taskName,
     dueDate: $dueDate,
-    priority: $priority,
     eta: $eta,
+    completionTime: $completionTime,
+    priority: $priority,
     completionStatus: $completionStatus
   }
-  
+  console.log(JSON.stringify(task));
   taskList.push(task);
 }
 
@@ -62,16 +63,17 @@ console.log(JSON.stringify(taskList));
 
 // Submit Button Event Listener
 
-//let button = document.getElementById("myButton");
+let button = document.getElementById("submitTaskPress");
 
-document.getElementById("submitTask").addEventListener("click", function() {
+button.addEventListener("click", function() {
   let $taskName = document.getElementById("taskName").value;
   let $dueDate = document.getElementById("dueDate").value;
-  let $eta = document.getElementById("eta").value;
-  let $priority = document.getElementById("priority").value;
-addTask($taskName, $dueDate, $eta, $priority);
+  let $eta = document.getElementById("etaInput").value;
+  let $completionTime = document.getElementById("completionTimeInput").value;
+  let $priority = document.getElementById("priorityInput").options[priorityInput.selectedIndex].value; 
+addTask($taskName, $dueDate, $eta, $completionTime, $priority);
 
-//console.log(JSON.stringify(taskList));
+console.log(JSON.stringify(taskList));
 
 aPlaceToShow.innerHTML = "";
 
@@ -89,3 +91,54 @@ aPlaceToShow.innerHTML = "";
  });
 
 });
+
+function renderTask(task){
+  //Create HTML elements
+  let item = document.createElement
+
+
+  //Extra task DOM elements
+
+  //Event Listeners for DOM elements
+
+  //Clear the input form
+
+
+}
+
+
+/***************************** 11 May 2021  ************************/
+/*************** CREATING ALL BOARDS  ******************************/
+let allBoards = [{
+      "label": "To Do"
+    },
+    {
+      "label": "Doing"
+    },
+    {
+      "label": "Review"
+    },
+    {
+      "label": "Done"
+    }
+  ];
+
+    /******** RENDERING */
+    let container = document.getElementById("container");
+
+    allBoards.forEach(function(board){
+      //console.log(board.label);
+
+      /* add a swimline (column) as a board */
+      let boardLane =  document.createElement('div');
+      boardLane.setAttribute('class','board');
+
+      /* add a label for the board */ 
+      let boardLabel =  document.createElement('div');
+      boardLabel.setAttribute('class','label');
+      boardLabel.innerHTML = board.label;
+
+      boardLane.appendChild(boardLabel);
+      container.appendChild(boardLane);
+   
+    });
